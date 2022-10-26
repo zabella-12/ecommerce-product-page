ce24a16c415a19843dcb85dc3ad7c63a71d36a69
Isabella <61935862+zabella-12@users.noreply.github.com>
2022-10-25 22:11:41 -0300

    corração de imagem e add toast

--- index.js
+++ index.js
@@ -2,14 +2,16 @@ let adiciona = document.getElementById('add');
 let remove = document.getElementById('del');
 let contador = 0;
 let newQtd = 0
-let shopping = document.getElementById('add-card')
+let shopping = document.querySelector('#add-card')
 let price = document.querySelector("h3.price").getAttribute("data-price")
 let cart = 0
 let notifica = document.getElementById('notificacao').style.display = "none"
 
+
 adiciona.addEventListener('click', function () {
     contador = contador + 1
     newQtd = document.getElementById('qtd').innerHTML = contador;
+
 })
 
 remove.addEventListener('click', function () {
@@ -30,8 +32,27 @@ function buy() {
     if (cart > 0) {
         notifica = document.getElementById('notificacao').style.display = "inline-block"
         notifica = document.getElementById('notificacao').innerHTML = shopping
+       
+        
     }
 }
+
+
+
+    //toast
+    const toastTrigger = document.getElementById('liveToastBtn')
+    const toastLiveExample = document.getElementById('liveToast')
+    if (toastTrigger) {
+        
+        toastTrigger.addEventListener('click', () => {
+    const toast = new bootstrap.Toast(toastLiveExample)
+
+    toast.show()
+
+        })
+        }
+
+
 
 function dump() {
     cart = 0
@@ -68,17 +89,17 @@ let thum04 = document.getElementById('thumb4')
 let fullImg = document.getElementById('bigImg')
 
 thum01.addEventListener('click', function(){
-    fullImg.innerHTML = `<img class=" w-75 formas-das-bordas"  src="/images/image-product-1.jpg"></img>` ;
+    fullImg.innerHTML = `<img class=" w-75 formas-das-bordas"  src="./images/image-product-1.jpg"></img>` ;
 })
 
 thum02.addEventListener('click', function(){
-    fullImg.innerHTML = `<img class=" w-75 formas-das-bordas"  src="/images/image-product-2.jpg"></img>` ;
+    fullImg.innerHTML = `<img class=" w-75 formas-das-bordas"  src="./images/image-product-2.jpg"></img>` ;
 })
 
 thum03.addEventListener('click', function(){
-    fullImg.innerHTML = `<img class=" w-75 formas-das-bordas"  src="/images/image-product-3.jpg"></img>` ;
+    fullImg.innerHTML = `<img class=" w-75 formas-das-bordas"  src="./images/image-product-3.jpg"></img>` ;
 })
 
 thum04.addEventListener('click', function(){
-    fullImg.innerHTML = `<img class=" w-75 formas-das-bordas"  src="/images/image-product-4.jpg"></img>` ;
+    fullImg.innerHTML = `<img class=" w-75 formas-das-bordas"  src="./images/image-product-4.jpg"></img>` ;
 })
